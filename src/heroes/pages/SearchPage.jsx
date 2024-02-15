b1787211b4f0e815f36ef219ab2897a7a0deb715
Iván Parada <ivan.parada2001@alumnos.ubiobio.cl>
2024-02-15 11:48:28 -0300

    condicionales search error

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -12,6 +12,9 @@ export const SearchPage = () => {
   const { q = '' } = queryString.parse(location.search)
   const heroes = getHeroByName(q)
 
+  const showSearch = (q.length === 0)
+  const showError = (q.length > 0) && heroes.length === 0
+
   const { searchText, onInputChange } = useForm({
     searchText: q,
 
@@ -19,7 +22,6 @@ export const SearchPage = () => {
 
   const onSearchSubmit = (event) => {
     event.preventDefault()
-    if (searchText.trim().length <= 1) return
 
     navigate(`?q=${searchText}`)
 
@@ -53,9 +55,17 @@ export const SearchPage = () => {
           <h4>Results</h4>
           <hr />
 
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? '' : 'none' }}>
+            Search a hero
+          </div>
+          <div
+            className="alert alert-danger  animate__animated animate__fadeIn"
+            style={{ display: showError ? '' : 'none' }}>
+            No hero with <b>{q}</b>
+          </div>
 
-          <div className="alert alert-primary">Search a hero</div>
-          <div className="alert alert-danger">No hero with <b>{q}</b></div>
           {
             heroes.map(hero => (
 
