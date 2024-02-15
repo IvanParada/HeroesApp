40d8249fcee79d2b567f59ed051af1bb598603f9
Iván Parada <ivan.parada2001@alumnos.ubiobio.cl>
2024-02-15 12:06:41 -0300

    context-reducer auth

---
+++ src/auth/context/AuthProvider.jsx
@@ -0,0 +1,17 @@
+import { useReducer } from "react"
+import { AuthContext, AuthReducer } from "./"
+
+const initialState = {
+    logged:false
+}
+
+export const AuthProvider = ({children}) => {
+
+   const [state, dispatch] = useReducer(AuthReducer, initialState)
+
+  return (
+    <AuthContext.Provider value={{}}>
+        {children}
+    </AuthContext.Provider>
+  )
+}
